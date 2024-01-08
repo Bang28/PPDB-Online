52c48e2f6da85f710684b5e1ea4e27bcc27db910
Bang28 <mhdmasruri9@gmail.com>
2024-01-08 12:23:58 +0700

    backend: finishing form pendaftaran siswa baru

--- PPDB/ppdb/views/formulir.py
+++ PPDB/ppdb/views/formulir.py
@@ -10,7 +10,12 @@ from django.views.decorators.cache import cache_control
 @login_required(login_url="login")
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def berkas(request):
-    '''fungsi menampilkan template Siswa datadiri'''
+    '''fungsi menampilkan form berkas'''
+
+    # cek data siswa
+    if Siswa.objects.filter(nisn=request.user).exists():
+        messages.info(request, 'Anda sudah terdaftar di PPDB tahun ini!')
+        return redirect('ppdb:dashboard')
 
     siswa = Siswa.objects.filter(nisn=request.user)
     
@@ -18,25 +23,35 @@ def berkas(request):
         form = BerkasForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            messages.success(request, "Data Berkas disimpan, silahkan lengkapi data berikut!")
+            messages.success(request, "Data anda sudah lengkap, silahkan tunggu verifikasi data dari pihak sekolah!")
             return redirect('ppdb:dashboard')
         else:
             for error in list(form.errors.values()):
                 messages.error(request, error)
-            return redirect('ppdb:berkas')
+            return redirect('ppdb:data-berkas')
     
+    # inisialisasi form
     form = BerkasForm(initial={'siswa':siswa})
     context = {
         'page_title': 'Siswa PPDB | SMP Miftahul Falah Gandrungmangu',
         'form': form,
-        'active': 'active',
+        'side_active': 'active',
+        'berkas_active': 'active',
+        'datadiri': ['active', 'done'],
+        'ayah': ['active', 'done'],
+        'ibu': ['active', 'done'],
+        'wali': ['active', 'done'],
     }
     return render(request, 'ppdb/forms/singleForm.html', context)
 
 @login_required(login_url="login")
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def dataWali(request):
-    '''fungsi menampilkan template Siswa datadiri'''
+    '''fungsi menampilkan form data wali'''
+
+    # cek data siswa
+    if Siswa.objects.filter(nisn=request.user).exists():
+        return redirect('ppdb:data-berkas')
 
     siswa = Siswa.objects.filter(nisn=request.user)
     
@@ -45,24 +60,33 @@ def dataWali(request):
         if form.is_valid():
             form.save()
             messages.success(request, "Data Wali disimpan, silahkan lengkapi data berikut!")
-            return redirect('ppdb:berkas')
+            return redirect('ppdb:data-berkas')
         else:
             for error in list(form.errors.values()):
                 messages.error(request, error)
             return redirect('ppdb:data-wali')
     
+    # inisialisasi form
     form = DataWaliForm(initial={'siswa':siswa})
     context = {
         'page_title': 'Siswa PPDB | SMP Miftahul Falah Gandrungmangu',
         'form': form,
-        'active': 'active',
+        'side_active': 'active',
+        'wali_active': 'active',
+        'datadiri': ['active', 'done'],
+        'ayah': ['active', 'done'],
+        'ibu': ['active', 'done'],
     }
     return render(request, 'ppdb/forms/singleForm.html', context)
 
 @login_required(login_url="login")
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def dataIbu(request):
-    '''fungsi menampilkan template Siswa datadiri'''
+    '''fungsi menampilkan form data ibu'''
+
+    # cek data siswa
+    if Siswa.objects.filter(nisn=request.user).exists():
+        return redirect('ppdb:data-wali')
 
     siswa = Siswa.objects.filter(nisn=request.user)
     
@@ -77,18 +101,26 @@ def dataIbu(request):
                 messages.error(request, error)
             return redirect('ppdb:data-ibu')
     
+    # inisialisasi form
     form = DataIbuForm(initial={'siswa':siswa})
     context = {
         'page_title': 'Siswa PPDB | SMP Miftahul Falah Gandrungmangu',
         'form': form,
-        'active': 'active',
+        'side_active': 'active',
+        'ibu_active': 'active',
+        'datadiri': ['active', 'done'],
+        'ayah': ['active', 'done'],
     }
     return render(request, 'ppdb/forms/singleForm.html', context)
 
 @login_required(login_url="login")
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def dataAyah(request):
-    '''fungsi menampilkan template Siswa datadiri'''
+    '''fungsi menampilkan data ayah'''
+
+    # cek data siswa
+    if Siswa.objects.filter(nisn=request.user).exists():
+        return redirect('ppdb:data-ibu')
 
     siswa = Siswa.objects.filter(nisn=request.user)
     
@@ -103,18 +135,25 @@ def dataAyah(request):
                 messages.error(request, error)
             return redirect('ppdb:data-ayah')
     
+    # inisialisasi form
     form = DataAyahForm(initial={'siswa':siswa})
     context = {
         'page_title': 'Siswa PPDB | SMP Miftahul Falah Gandrungmangu',
         'form': form,
-        'active': 'active',
+        'side_active': 'active',
+        'datadiri': ['active', 'done'],
+        'ayah_active':'active'
     }
     return render(request, 'ppdb/forms/singleForm.html', context)
 
 @login_required(login_url="login")
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def dataDiri(request):
-    '''fungsi menampilkan template Siswa datadiri'''
+    '''fungsi menampilkan data diri'''
+
+    # cek data siswa
+    if Siswa.objects.filter(nisn=request.user).exists():
+        return redirect('ppdb:data-ayah')
 
     nisn = request.user 
     thn = TahunAjaran.objects.filter(status="Dibuka").last()
@@ -130,10 +169,13 @@ def dataDiri(request):
                 messages.error(request, error)
             return redirect('ppdb:data-diri')
     
+    # inisialisasi form
     form = SiswaForm(initial={'nisn':nisn, 'thn_ajaran':thn})
     context = {
         'page_title': 'Siswa PPDB | SMP Miftahul Falah Gandrungmangu',
         'form': form,
+        'side_active': 'active',
+        'active': 'active',
     }
     return render(request, 'ppdb/forms/singleForm.html', context)
 
@@ -142,6 +184,7 @@ def dataDiri(request):
 def dataMaster(request, pk):
     '''fungsi view data dan edit data master peserta berdasarkan request user'''
 
+    # cek data siswa
     try:
         data = Siswa.objects.filter(nisn=request.user).get(nisn=pk)
     except Siswa.DoesNotExist:
